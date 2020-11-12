6d53e0afc1da0091aaebbb8449ad7e21e8ef2bc2
Eric Weise <ericdweise@gmail.com>
2020-11-12 10:15:14 -0800

    revamp build script

--- build-helper.py
+++ build-helper.py
@@ -1,22 +1,38 @@
 import os
 import sys
 
-input_file = sys.argv[1]
-output_file = sys.argv[2]
 
-HEADERFILE = 'assets/header.html'
-FOOTERFILE = 'assets/footer.html'
+fHEAD = 'assets/head.html'
+fHEADER = 'assets/header.html'
+fFOOTER = 'assets/footer.html'
 
-with open(input_file, 'r') as fin:
-    with open(output_file, 'w') as fout:
+
+def copy_part(source, target_fp):
+    assert(os.path.exists(source)), 'Cannot find input file: ' + source
+    with open(source, 'r') as fin:
         for line in fin:
-            if 'HEADER' in line:
-                with open(HEADERFILE, 'r') as fhead:
-                    for hline in fhead:
-                        fout.write(hline)
-            elif 'FOOTER' in line:
-                with open(FOOTERFILE, 'r') as ffoot:
-                    for fline in ffoot:
-                        fout.write(fline)
-            else:
-                fout.write(line)
+            target_fp.write(line)
+
+
+def main(source, target):
+    assert(os.path.exists(source)), 'Sourcefile does not exist: ' + source
+
+    with open(target, 'w') as fout:
+        fout.write('<!DOCTYPE html>\n')
+        fout.write('<html lang="en">\n')
+        copy_part(fHEAD, fout)
+        fout.write('<body>\n')
+        fout.write('<div class="contents">\n')
+        copy_part(fHEADER, fout)
+        fout.write('<div class="detail">\n')
+        copy_part(source, fout)
+        fout.write('</div> <!-- class="detail" -->\n')
+        copy_part(fFOOTER, fout)
+        fout.write('</div> <!-- class="contents" -->\n')
+        fout.write('</body>\n')
+        fout.write('</html>\n')
+
+if __name__ == '__main__':
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+    main(input_file, output_file)
--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ set -e
 # first version
 # must run from this directory
 
-for IN in $(find . -type f -name '*.stub'); do
-	OUT=$(echo $IN | sed -r 's/.stub/.html/')
+for IN in $(find . -type f -name '*.stub.html'); do
+	OUT=$(echo $IN | sed -r 's/.stub//')
 	echo "*** $IN >>> $OUT"
 	python build-helper.py $IN $OUT
 done
